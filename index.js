74a47604ae9bd899ad88c22137f4418e5e5b1c71
katyusya <katusha.fedyakena@mail.ru>
2023-10-22 00:52:21 +0300

    Добавление Experts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import './src/styles/main.less';
 import './src/styles/banner.less';
 import './src/styles/experience.less';
+import './src/styles/experts.less';
 import './src/styles/header.less';
 import './src/styles/footer.less';
 
