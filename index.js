5c9136933a65e25af7c298a3c7328cc5f039de1e
katyusya <katusha.fedyakena@mail.ru>
2023-10-22 00:21:32 +0300

    Добавление Experience

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { ANSWER } from './module';
 import $ from 'jquery';
 import './src/styles/main.less';
 import './src/styles/banner.less';
+import './src/styles/experience.less';
 import './src/styles/header.less';
 import './src/styles/footer.less';
 
