3807894e9f1772989c9817c737be6f33715a7cb8
katyusya <katusha.fedyakena@mail.ru>
2023-10-21 18:28:59 +0300

    Инициализация проекта

---
+++ index.js
@@ -0,0 +1,9 @@
+import { ANSWER } from './module';
+// import $ from 'jquery';
+import './src/styles/main.less';
+
+console.log('Universe answer: ', ANSWER);
+
+// $(function () {
+//   console.log('Готов!');
+// });
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    host: '192.168.31.144',
+    port: 5500,
+  },
+};
