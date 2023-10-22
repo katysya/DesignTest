3860c3710b552c5abe6ec9bf15806d2ece2f4092
katyusya <katusha.fedyakena@mail.ru>
2023-10-22 20:09:00 +0300

    Адаптив Speakers

--- index.js
+++ index.js
@@ -16,9 +16,36 @@ $(function () {
 
   $('.swiper').slick({
     centerMode: true,
+    dots: false,
     slidesToShow: 3,
     slidesToScroll: 3,
     nextArrow: document.querySelector('.swiper__next'),
     prevArrow: document.querySelector('.swiper__prev'),
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
