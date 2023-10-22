e479d8b35b97e512d4d67452ae23c41713bbb71c
katyusya <katusha.fedyakena@mail.ru>
2023-10-22 14:55:45 +0300

    Добавление Form

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { ANSWER } from './module';
 import $ from 'jquery';
 import './src/styles/main.less';
 import './src/styles/banner.less';
+import './src/styles/header.less';
 import './src/styles/experience.less';
 import './src/styles/experts.less';
-import './src/styles/header.less';
+import './src/styles/participation.less';
 import './src/styles/footer.less';
 
 console.log('Universe answer: ', ANSWER);
